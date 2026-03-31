3174e8fd5f42d34bf8eae0379343659ed31e3fab
Angel Ysmael <angelysmael7@gmail.com>
2026-03-31 18:40:07 -0400

    Configure Github Pages deployment

--- data/sneakers.js
+++ data/sneakers.js
@@ -1,14 +1,17 @@
+const basePath =
+  process.env.NODE_ENV === "production" ? "/sneaker-vault" : "";
+
 export const sneakers = [
-  { id: 1, name: "Air Max 95", price: 150, image: "/am95.png" },
-  { id: 2, name: "Jordan 4", price: 300, image: "/j4.png" },
-  { id: 3, name: "Jordan 1", price: 280, image: "/j1.png" },
-  { id: 4, name: "Yeezy Boost 350", price: 220, image: "/yeezy350.png" },
-  { id: 5, name: "Yeezy Boost 700", price: 300, image: "/yeezy700.png" },
-  { id: 6, name: "Nike Dunk Low", price: 150, image: "/dunklow.png" },
-  { id: 7, name: "Nike Air Force 1", price: 160, image: "/af1.png" },
-  {id: 8, name: "Adidas Superstar", price: 85, image: "/superstar.png" },
-  {id: 9, name: "Nike Shox TL", price: 185, image: "/shox.png" },
-  {id: 10, name: "Jordan 6", price: 300, image: "/j6.png" },
-  {id: 11, name: "Jordan 11", price: 300, image: "/j11.png" },
-  {id: 12, name: "Jordan 3", price: 140, image: "/j3.png" },
+  { id: 1, name: "Air Max 95", price: 150, image: `${basePath}/am95.png` },
+  { id: 2, name: "Jordan 4", price: 300, image: `${basePath}/j4.png` },
+  { id: 3, name: "Jordan 1", price: 280, image: `${basePath}/j1.png` },
+  { id: 4, name: "Yeezy Boost 350", price: 220, image: `${basePath}/yeezy350.png` },
+  { id: 5, name: "Yeezy Boost 700", price: 300, image: `${basePath}/yeezy700.png` },
+  { id: 6, name: "Nike Dunk Low", price: 150, image: `${basePath}/dunklow.png` },
+  { id: 7, name: "Nike Air Force 1", price: 160, image: `${basePath}/af1.png` },
+  { id: 8, name: "Adidas Superstar", price: 85, image: `${basePath}/superstar.png` },
+  { id: 9, name: "Nike Shox TL", price: 185, image: `${basePath}/shox.png` },
+  { id: 10, name: "Jordan 6", price: 300, image: `${basePath}/j6.png` },
+  { id: 11, name: "Jordan 11", price: 300, image: `${basePath}/j11.png` },
+  { id: 12, name: "Jordan 3", price: 140, image: `${basePath}/j3.png` },
 ];
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",              
+  basePath: "/sneaker-vault",   
+  assetPrefix: "/sneaker-vault/", 
 };
 
 export default nextConfig;
